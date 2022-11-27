731ed2891e3a0797864f528f2936eebadbf2b217
Tejas Nikam <Tejasnikam4515@gmail.com>
2022-11-28 01:25:36 +0530

    Information Updated.
    Name,Phone,Image and email changed.

--- src/contacts.js
+++ src/contacts.js
@@ -1,23 +1,23 @@
 const contacts = [
   {
-    name: "Beyonce",
+    name: "Tejas Nikam",
     imgURL:
-      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
-    phone: "+123 456 789",
-    email: "b@beyonce.com"
+      "https://picsum.photos/200/300",
+    phone: "+91 9595729100",
+    email: "contactTejas@gmail.com"
   },
   {
-    name: "Jack Bauer",
+    name: "Suraj More",
     imgURL:
-      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
-    phone: "+987 654 321",
-    email: "jack@nowhere.com"
+      "https://picsum.photos/200/300",
+    phone: "+91 9595729100",
+    email: "contactSuraj@gmail.com"
   },
   {
-    name: "Chuck Norris",
+    name: "Suraj Mahind",
     imgURL:
-      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
-    phone: "+918 372 574",
-    email: "gmail@chucknorris.com"
+      "https://picsum.photos/200/300",
+    phone: "+91 9595729100",
+    email: "contactSurajpm@gmail.com"
   }
 ];
